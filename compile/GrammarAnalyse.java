aa0b00f31703668a8efab425721ad1412f38ff3c
lujiamin <1207198860@qq.com>
2017-06-02 13:14:09 +0800

    follows

--- compile/GrammarAnalyse.java
+++ compile/GrammarAnalyse.java
@@ -8,23 +8,35 @@ import java.util.HashSet;
 public class GrammarAnalyse {
 
 	// 假定用 $ 符号代表空 ε
-	private static String[] arr = { "A->[B", "B->X]|BA", "X->Xa|Xb|a|b" };
+	private static String[] arrRaw = { "E->E+T|T", "T->T*F|F", "F->(E)|i" };// "E->TA",
+																			// "A->+TA|$",
+																			// "T->FB",
+																			// "B->*FB|$",
+																			// "F->(E)|i"
 
 	public static void main(String[] args) {
 		System.out.println("the raw rules:");
-		for (String s : arr) {
+		for (String s : arrRaw) {
 			System.out.println(s);
 		}
-		HashMap<Character, ArrayList<String>> map = removeRecur(arr);
+		System.out.println();
+		HashMap<Character, ArrayList<String>> map = removeRecur(arrRaw);
 		System.out.println("the map after removing Recur:");
 		System.out.println(map);
+		System.out.println();
 		String[] arrC = mapToString(map);
 		System.out.println("the rules after removing recur:");
 		for (String s : arrC) {
 			System.out.println(s);
 		}
-		System.out.println("the set of firsts:");
+		System.out.println();
+		System.out.println("the map of firsts:");
 		System.out.println(getFirsts(arrC));
+		System.out.println();
+		System.out.println("the map of follows");
+		System.out.println(getFollows(arrC));
+		System.out.println("**************************************************");
+		analyseTable(arrC);
 	}
 
 	// 拆分规则
@@ -179,6 +191,108 @@ public class GrammarAnalyse {
 			firsts.put(ch, setFirsts);
 		}
 		return firsts;
+	}
+
+	public static HashMap<Character, HashSet<Character>> getFollows(String[] arr) {
+		HashMap<Character, HashSet<Character>> follows = new HashMap<>();
+		HashMap<Character, ArrayList<String>> mapRules = new HashMap<>();
+		HashMap<Character, HashSet<Character>> firsts = new HashMap<>();
+		HashSet<String> set = new HashSet<>();// set 保存规则右部
+		// 获取各项规则转化为 map 类型
+		mapRules = splitRules(arr);
+		firsts = getFirsts(arr);
+		ArrayList<String> ruleList = new ArrayList<>();
+		for (Character ch : mapRules.keySet()) {
+			ruleList.addAll(mapRules.get(ch));
+		}
+		for (String s : ruleList) {
+			// 判断字符串是否包含相同内容
+			if (!s.equals("$")) {
+				set.add(s);
+			}
+		}
+		// System.out.println(set);
+		for (Character ch : mapRules.keySet()) {
+			HashSet<Character> setFollows = new HashSet<>();
+			// 求Follows集的规则1. 如果是初始字符，则将 # 加入Follows集
+			if (ch == arrRaw[0].charAt(0)) {
+				setFollows.add('#');
+			}
+			// 求Follows集的规则2
+			for (String s : set) {
+				int chIndex = s.indexOf(ch);
+				if (chIndex >= 0 && chIndex <= s.length() - 2 && s.charAt(chIndex + 1) != '$') {
+					// 下一个字符是终结符
+					if (isTelement(arr, s.charAt(chIndex + 1))) {
+						setFollows.add(s.charAt(chIndex + 1));
+					} else {
+						setFollows.addAll(firsts.get(s.charAt(chIndex + 1)));
+					}
+				}
+
+			}
+			if (follows.containsKey(ch)) {
+				setFollows.addAll(follows.get(ch));
+			}
+			// 移除空串 $
+			setFollows.remove('$');
+			follows.put(ch, setFollows);
+		}
+		// System.out.println(follows);
+		int i = 0;// 设置反复执行规则的次数,只能使用5次做一个大概的估算
+		while (i < 5) {
+			for (Character ch : mapRules.keySet()) {
+				HashSet<Character> setFollows = new HashSet<>();
+				// 尝试规则3
+				for (String s : mapRules.get(ch)) {
+					// 满足A->aB形式
+					if (!isTelement(arr, s.charAt(s.length() - 1))) {
+						setFollows.addAll(follows.get(ch));
+						if (follows.containsKey(s.charAt(s.length() - 1))) {
+							setFollows.addAll(follows.get(s.charAt(s.length() - 1)));
+						}
+						follows.put(s.charAt(s.length() - 1), setFollows);
+					}
+
+				}
+			}
+			// System.out.println(follows);
+			for (Character ch : mapRules.keySet()) {
+				HashSet<Character> setFollows = new HashSet<>();
+				for (String s : mapRules.get(ch)) {
+					// 满足A->aBb形式 且 经过一定步骤 b->$
+					if (s.length() >= 2 && !isTelement(arr, s.charAt(s.length() - 2))) {
+						// 规则中的 $ 是字符串 ,不是字符
+						if (!isTelement(arr, s.charAt(s.length() - 1))
+								&& mapRules.get(s.charAt(s.length() - 1)).contains("$")) {
+
+							setFollows.addAll(follows.get(ch));
+							if (follows.containsKey(s.charAt(s.length() - 2))) {
+								setFollows.addAll(follows.get(s.charAt(s.length() - 2)));
+							}
+							follows.put(s.charAt(s.length() - 2), setFollows);
+						}
+					}
+				}
+			}
+			i++;
+		}
+		return follows;
+
+	}
+
+	public static void analyseTable(String[] arr) {
+		HashMap<Character, HashSet<Character>> firsts = new HashMap<>();
+		HashMap<Character, HashSet<Character>> follows = new HashMap<>();
+		HashSet<Character> Telem = new HashSet<>();
+		HashSet<Character> Nelem = new HashSet<>();
+		firsts = getFirsts(arr);
+		follows = getFollows(arr);
+		Telem = getTelements(arr);
+		Nelem = getNelements(arr);
+		System.out.println(Telem);
+		System.out.println(Nelem);
+
 	}
 
 }
